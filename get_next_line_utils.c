88020d3de4d8b66ca8053326cab2ecad2ed476e8
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2021-12-09 20:54:47 +0900

    first draft

---
+++ get_next_line_utils.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hokutosuzuki <hosuzuki@student.42toky      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 12:17:52 by hokutosuz         #+#    #+#             */
+/*   Updated: 2021/12/09 19:12:59 by hokutosuz        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	size_t	i;
+	size_t	j;
+
+	dst = NULL;
+	if (!s1 || !s2)
+		return (NULL);
+	dst = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		dst[i++] = s2[j++];
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != (char)c && s[i] != '\0')
+		i++;
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strndup(const char *src, size_t n)
+{
+	size_t	len;
+	char	*dst;
+
+	dst = (char *)malloc(n + 1);
+	if (!dst)
+		return (NULL);
+	len = 0;
+	while (len < n)
+	{
+		dst[len] = src[len];
+		len++;
+	}
+	dst[len] = '\0';
+	return (dst);
+}
+
+t_list	*ft_lstnew(int fd, char *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->fd = fd;
+	new->str = ft_strndup(content, ft_strlen(content));
+	new->next = NULL;
+	return (new);
+}
